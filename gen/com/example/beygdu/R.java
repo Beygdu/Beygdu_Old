9ecd7519a52f1642e93225dd826df0a56bf2bc8b
Jón Friðrik Jónatansson <jfjclarke@gmail.com>
2014-10-12 22:14:17 +0000

    Vantaði style.xml

--- gen/com/example/beygdu/R.java
+++ gen/com/example/beygdu/R.java
@@ -24,14 +24,11 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
-        public static final int border_offset=0x7f050003;
-        public static final int border_width=0x7f050002;
     }
     public static final class drawable {
         public static final int border=0x7f020000;
         public static final int button=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int tableborder=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f090004;
@@ -48,7 +45,6 @@ public final class R {
     public static final class menu {
         public static final int beygingar=0x7f080000;
         public static final int main=0x7f080001;
-        public static final int table_test=0x7f080002;
     }
     public static final class string {
         public static final int action_settings=0x7f060002;
